d0f2b8c8a2c81d27f170096b4a1460331230ffff
tbsilveira <tiagobsilveira@gmail.com>
2022-11-04 20:17:26 +0000

    New routes added

--- api/index.js
+++ api/index.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require ('express');
+const routes = require ('./routes');
 
 const app = express();
-
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get('/teste', (req, res) => {
-  res.status(200).send({message: 'boas-vidas à API'})
-});
+routes(app);
 
 app.listen(port, () => {
-  console.log(`O servidor está rodando na porta ${port}`)
+  console.log(`O servidor está rodando na porta ${port}`);
 });
 
 module.exports = app;
