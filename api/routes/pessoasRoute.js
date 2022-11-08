c154b17d3ded88ba8d05a6dd0f48776c70489c9a
tbsilveira <tiagobsilveira@gmail.com>
2022-11-08 12:32:31 +0000

    Methods and routes added for Matriculas table

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,5 +8,9 @@ router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 router.post('/pessoas/', PessoaController.criaPessoa);
 router.put('/pessoas/:id', PessoaController.atualizaPessoa);
 router.delete('/pessoas/:id', PessoaController.deletaPessoa);
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deletaMatricula);
 
 module.exports = router;
